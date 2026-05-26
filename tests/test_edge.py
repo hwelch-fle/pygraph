9520fa6882ebecc5f33dde1f318e7efa04ca818b
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-05-26 11:39:57 -0400

    Fix formatting in tests

--- tests/test_edge.py
+++ tests/test_edge.py
@@ -4,21 +4,21 @@ from pygraph import Edge
 
 
 def test_edge_init():
-    e = Edge(0,0, color='red')
+    e = Edge(0, 0, color='red')
     assert 'color' in e.data
     assert e.data['color'] == 'red'
     assert 'arrows' not in e.data
-    assert e.key == (0,0)
+    assert e.key == (0, 0)
 
     with pytest.raises(TypeError):
-        e = Edge(1,2,4) # type: ignore
+        e = Edge(1, 2, 4)  # type: ignore
 
-    e = Edge(1,2, id='Edge_ID')
+    e = Edge(1, 2, id='Edge_ID')
     assert e['id'] == 'Edge_ID'
 
 
 def test_edge_set():
-    e = Edge(0,0)
+    e = Edge(0, 0)
     e.set(color='red', label="I'm Red", arrows='to;from')
     assert 'color' in e.data
     assert e.data['color'] == 'red'
@@ -29,12 +29,12 @@ def test_edge_set():
 
 
 def test_edge_getitem():
-    e = Edge(0,0, color='red', arrows='to;from')
+    e = Edge(0, 0, color='red', arrows='to;from')
     assert e['color'] == 'red'
     assert e['arrows'] == 'to;from'
 
 
 def test_edge_setitem():
-    e = Edge(0,0, color='red', arrows='to;from')
+    e = Edge(0, 0, color='red', arrows='to;from')
     e['color'] = 'blue'
     assert e.data.get('color') == 'blue'
