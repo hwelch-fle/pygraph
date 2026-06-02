1ba491aa7b355a660a0fe1787e5b6e0da03faf5e
hwelch-fle <hwelch@finelines-engineering.com>
2026-06-02 01:19:42 -0400

    Add more example graphs

--- dev/build_graph.py
+++ dev/build_graph.py
@@ -15,9 +15,9 @@ def main():
         style=GitRepoStyle,
         ignores={'files': ['.gitignore'], 'patterns': ['.venv/', '.git/']},
     )
-    nw = builder.web_network(host='github.com', user='hwelch-fle', branch='master')
+    nw = builder.web_network()
     print(nw)
-    out = Path('docs/ref/index.html')
+    out = Path(f'docs/ref/examples/{builder.repo}.html')
     out.parent.mkdir(parents=True, exist_ok=True)
     out.write_text(to_html(nw, template, jinja={'background': '#1f1f1f'}))
 
